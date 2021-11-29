6c98691ace5232c47e48c6dc8b3751f12faf0fdf
Guilhermergit <57076576+Guilhermergit@users.noreply.github.com>
2021-11-29 02:34:20 -0300

    Delete Aula_Anderson_Projeto_de_BI-main directory

--- Aula_Anderson_Projeto_de_BI-main/README.md
+++
@@ -1,8 +0,0 @@
-# Aula_Anderson_Projeto_de_BI
-Tema do projeto: Loja de brinquedos infantis
-
-Entrega inicial: 30/09/2021
-
-ETL Criado.
-
-Integrantes: Francisco Di Lazaro, Guilherme Rodrigues.
--- Aula_Anderson_Projeto_de_BI-main/Scripts/Transacional/povoamento_tipo_produto.sql
+++
@@ -1,6 +0,0 @@
-insert into tipo_produto (cod_tipo, nm_tipo) values (1, 'boneco');
-insert into tipo_produto (cod_tipo,nm_tipo) values (2, 'triciclo');
-insert into tipo_produto (cod_tipo,nm_tipo) values (3, 'pelucia');
-insert into tipo_produto (cod_tipo,nm_tipo) values (4, 'jogo de tabuleiro');
-insert into tipo_produto (cod_tipo,nm_tipo) values (5, 'diversos');
-
